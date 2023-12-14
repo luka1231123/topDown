31f719576b18bb92268e29c27822983f7178f074
luka1231123 <luka04rekhviashvili@gmail.com>
2023-12-14 23:12:46 +0400

    aa

--- Program.cs
+++ Program.cs
@@ -5,49 +5,23 @@ using System.Collections.Specialized;
 using System.Drawing;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using static topDown.ExtraMethods;
+ using topDown.Main;
+ using static topDown.ExtraMethods;
 
 namespace topDown;
 public class Program
 {
     
-    static Window wnd = new Window();
-    static vec3 color = new vec3 (0, 0, 0);
-    static Random rnd = new Random();
-    
+    private static MainLog MainApp = new MainLog();
+    private static Background backApp = new Background();
     static void Main()
     {
-        double c=0;
-        double v=0;
-
-        while (wnd.windOpen())
+        while (MainApp.WindOpen())
         {
-            wnd.Refresh();
-
-            //events here
-            for (int i = 0; i < 18; i++)
-            {
-                for (int j = 0; j < 18; j++)
-                {
-                    c=Convert.ToDouble(i);
-                    c=c/18;
-                    v=Convert.ToDouble(j);
-                    v=v/18;
-                    paintTile(i,j,"",new vec3(((v+c)/2),v,c));
-                }
-            }
-
-            wnd.Draw();
-        }
-        
-    }
-    static void paintTile(int tileX, int tileY, string path, vec3 color)
-    {
-        
-        for(int i=0; i<32; i++){
-            for(int j=0;j<32;j++){
-                wnd.SetPixel((uint)(i+(tileX*32)+448), (uint)(j+(tileY*32)), color);
-            }
+            backApp.fullScreen();
+            MainApp.Draw();
+            MainApp.Refresh();
         }
     }
+
 }
